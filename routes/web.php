865e0f62144ceba79c58b7513e79bef24f96614a
Jesray Garciano <jesray.garciano@gmail.com>
2018-10-01 16:52:32 +0800

    Added authentication

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Customer\Models\User;
+use Hyn\Tenancy\Models\Customer;
+
+Route::get('customers', function () {
+    return Customer::all();
+});
+
+Route::get('users', function () {
+    return User::all();
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
